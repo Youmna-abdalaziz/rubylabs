b17870b98c0ad786f0d608a7fc7e4e8ed1157e4d
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-04-20 16:30:17 +0200

    lab3 Autherization_part

--- lab3_blog/app/controllers/articles_controller.rb
+++ lab3_blog/app/controllers/articles_controller.rb
@@ -1,24 +1,33 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!
+  load_and_authorize_resource
   #http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
     def index
         @articles = Article.all
+        # authorize! :read, @article
     end
     
     def show
         @article = Article.find(params[:id])
+        authorize! :read, @article
     end
     
     def new
         @article = Article.new
+        authorize! :create, @article
+
     end
 
     def edit
         @article = Article.find(params[:id])
+        authorize! :edit, @article
     end
 
     def create
         @article = Article.new(article_params)
+        @article.user_id = current_user.id
+        #respond_with Article.create(article_params.merge(user_id: current_user.id))
+        authorize! :create, @article        
         if @article.save
             redirect_to @article
           else
@@ -29,6 +38,7 @@ class ArticlesController < ApplicationController
 
     def update
         @article = Article.find(params[:id])
+        authorize! :edit, @article
        
         if @article.update(article_params)
           redirect_to @article
@@ -41,6 +51,7 @@ class ArticlesController < ApplicationController
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
+        authorize! :destroy, @article
        
         redirect_to articles_path
     end
--- lab3_blog/app/controllers/comments_controller.rb
+++ lab3_blog/app/controllers/comments_controller.rb
@@ -2,12 +2,19 @@ class CommentsController < ApplicationController
     http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
     def create
         @article = Article.find(params[:article_id])
-        @comment = @article.comments.create(comment_params)
+        @comment = @article.comments.create(comment_params.merge!(user_id: current_user.id))
+        # @comment. = current_user.id
+        # @comment = @article.comments.create!(comment_params.merge!)
+        
+        
+        #respond_with @article, @comment
+        authorize! :create, @comment
         redirect_to article_path(@article)
     end
     def destroy
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
+        authorize! :destroy, @comment
         @comment.destroy
         redirect_to article_path(@article)
     end
