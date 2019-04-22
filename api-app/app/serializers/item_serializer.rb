56205c30b517bb4d5a36a72aa06820d099c147ee
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-04-22 16:11:09 +0200

    customize the output response

--- api-app/app/serializers/item_serializer.rb
+++ api-app/app/serializers/item_serializer.rb
@@ -1,4 +1,21 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description
-  
+  #attributes :id, :item_name, :description
+  attributes :item_name, :item_description
+  #def initialize(item, scope)
+   # @item, @scope = item, scope
+  #end
+
+  #def serializable_hash
+   # { item_name: @item.name, item_description: @item.description }
+  #end
+
+  #def as_json
+   # { item: serializable_hash }
+  #end
+  def item_name
+    object.name.upcase
+  end
+  def item_description
+    object.description.upcase
+  end 
 end
