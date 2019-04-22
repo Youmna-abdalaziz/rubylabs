f61c687207c9ba2826a8f149f1f2216843134e80
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-04-22 16:51:56 +0200

    implements post put delete methods

--- api-app/app/controllers/items_controller.rb
+++ api-app/app/controllers/items_controller.rb
@@ -7,4 +7,44 @@ class ItemsController < ApplicationController
         @items = Item.all
         render json: @items
     end
+    def new
+        @item = Item.new
+      end
+    
+      def edit
+      end
+
+    def create
+        @item = Item.new(item_params)
+        
+        if @item.save
+            render json: @item, status: :created
+        else
+            render json: { errors: @item.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @item = Item.find(params[:id])
+        
+        if @item.update(item_params)
+            render json: @item, status: :ok
+        else
+            render json: { errors: @item.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @item = Item.find(params[:id])
+        @item.destroy
+        head 204
+        end
+        
+        private
+            def item_params
+                params.require(:item).permit(:name, :description)
+            end
+
+       
+      
 end
