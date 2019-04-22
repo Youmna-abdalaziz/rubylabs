d92f7300932c6b3bd03f752ad58f78a7a4b0ad3e
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-04-22 15:32:35 +0200

    Implement serializer

--- api-app/db/seeds.rb
+++ api-app/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+item1 = Item.create({name: "Item1", description: "desc of 1st item"})
+item2 = Item.create({name: "Item2", description: "desc of 2nd item"})
+item3 = Item.create({name: "Item3", description: "desc of 3rd item"})
+item4 = Item.create({name: "Item4", description: "desc of 4th item"})
+item5 = Item.create({name: "Item5", description: "desc of 5th item"})
+item6 = Item.create({name: "Item6", description: "desc of 6th item"})
+item7 = Item.create({name: "Item7", description: "desc of 7th item"})
+item8 = Item.create({name: "Item8", description: "desc of 8th item"})
+item9 = Item.create({name: "Item9", description: "desc of 9th item"})
+item10 = Item.create({name: "Item10", description: "desc of 10th item"})
